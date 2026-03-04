acc0253c47675a0940485c1908d11c3148394323
Vladislav-Evg-Sid <vlad.s200@mail.ru>
2026-03-04 08:59:19 +0500

    Update __pycache__ in gitignore

--- app/api/qdrant.py
+++ app/api/qdrant.py
@@ -9,19 +9,19 @@ router = APIRouter(
 )
 
 # Взаимодействие с qdrant
-@router.post("/qdrant", response_model=QdrantAddReportResponse)
+@router.post("/add", response_model=QdrantAddReportResponse)
 async def qdrant_set_data(data: QdrantAddReportRequest, qd_manager: QdrantReportsService = Depends(get_qdrant_report_service)) -> QdrantAddReportResponse:
     """Добавление отчёта в векторную БД"""
     return await qd_manager.add_report(data)
 
 
-@router.get("/qdrant/reports", response_model=QdrantAllReportsResponse)
+@router.get("/reports", response_model=QdrantAllReportsResponse)
 async def get_all_reports(qd_manager: QdrantReportsService = Depends(get_qdrant_report_service)) -> QdrantAllReportsResponse:
     """Получение списка всех отчётов"""
     return await qd_manager.get_all_reports()
 
 
-@router.get("/qdrant/reports/{report_id}")
+@router.get("/reports/{report_id}")
 async def get_report_by_id(report_id: str, qd_manager: QdrantReportsService = Depends(get_qdrant_report_service)):
     """Получение полных данных отчёта по ID"""
     report = await qd_manager.get_report_by_id(report_id)
@@ -30,12 +30,12 @@ async def get_report_by_id(report_id: str, qd_manager: QdrantReportsService = De
     return report
 
 
-@router.delete("/qdrant/reports/{report_id}", response_model=QdrantDeleteReportResponse)
+@router.delete("/reports/{report_id}", response_model=QdrantDeleteReportResponse)
 async def delete_report(report_id: str, qd_manager: QdrantReportsService = Depends(get_qdrant_report_service)) -> QdrantDeleteReportResponse:
     """Удаление отчёта по ID"""
     return await qd_manager.delete_report(report_id)
 
 
-@router.post("/qdrant/reports/distance")
+@router.post("/reports/distance")
 async def get_distance(report1_id: str, report2_id: str, section_code: str, qd_manager: QdrantReportsService = Depends(get_qdrant_report_service)) -> QdrantReportSectionsComparisonResponce:
     return await qd_manager.get_distance(report1_id, report2_id, section_code)
